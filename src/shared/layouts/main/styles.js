29f8f403f538b1d3d7e31e0984e9a02d2c7aa05b
Mariana Moreira <marianaccmoreira@gmail.com>
2021-11-13 15:23:05 +0000

    fix main layout styling

--- src/shared/layouts/main/styles.js
+++ src/shared/layouts/main/styles.js
@@ -2,16 +2,16 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-height: "100%",
-width: "100%",
-display: "flex",
-overflow: "hidden"
+  height: 100%;
+  width: 100%;
+  display: flex;
+  overflow: hidden;
 `;
 
 const StyledWrapper = styled.div`
-width: "100%",
-display: "flex",
-flex: "1 1 auto"
+  width: 100%;
+  display: flex;
+  flex: 1 1 auto;
 `;
 
 export { StyledContainer, StyledWrapper };

6901b0b3c854d269fc35ff726b8ec867421f18a7
Mariana Moreira <marianaccmoreira@gmail.com>
2021-11-13 14:47:56 +0000

    config token redirect

--- src/config/app.js
+++ src/config/app.js
@@ -1,10 +1,28 @@
 // base
 import React from "react";
 
+// libraries
+import { Redirect, useLocation } from "react-router-dom";
+
 // routes
 import Routes from "routes/routes";
 
+// utils
+import { getJWT } from "shared/utils/jwt";
+
 const App = () => {
+  const location = useLocation();
+
+  const token = getJWT();
+
+  if (!token && location.pathname !== "/signin") {
+    return <Redirect to="/signin" />;
+  }
+
+  if (token && location.pathname === "/signin") {
+    return <Redirect to="/" />;
+  }
+
   return <Routes />;
 };
 

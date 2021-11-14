495e1386c935543801ce119cc71fed71e025ae2d
Mariana Moreira <marianaccmoreira@gmail.com>
2021-11-14 15:10:51 +0000

    update theme styling

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,14 +3,28 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   body {
-    background-color: ${({ theme }) => theme.colors.background};
-    color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colours.background};
+    color: ${({ theme }) => theme.colours.black};
     transition: background 0.2s ease-in, color 0.2s ease-in;
     font-family: 'Roboto', sans-serif;
   }
   
   .MuiPaper-root {
     border-radius: 0 !important;
+    color: ${({ theme }) => `${theme.colours.black} !important`} ;
+  }
+
+  .MuiFormControl-root {
+    display: block !important;
+  }
+
+  .MuiPickersDay-root {
+    color: ${({ theme }) => `${theme.colours.black} !important`} 
+  }
+  
+  .Mui-selected {
+    background-color: ${({ theme }) => theme.colours.secondary} !important;
+    color: ${({ theme }) => `${theme.colours.white} !important`};
   }
 `;
 
@@ -23,19 +37,19 @@ const general = {
 };
 
 const Light = {
-  colors: {
+  colours: {
     background: "rgba(244, 244, 244, 1)",
     inputBackground: "rgba(252, 252, 252, 1)",
     primary: "rgba(10, 72, 99, 1)",
-    secondary: "rgba(159, 185, 202, 1)",
-    lightSecondary: "rgba(221, 229, 234, 1)",
-    accent: "rgba(255, 200, 50, 1)",
+    secondary: "rgba(171, 195, 211, 1)",
+    lightSecondary: "rgba(210, 220, 227, 1)",
+    accent: "rgba(255, 208, 79, 1)",
     ...general
   }
 };
 
 const Dark = {
-  colors: {
+  colours: {
     background: "rgba(244, 244, 244, 1)",
     inputBackground: "rgba(252, 252, 252, 1)",
     primary: "rgba(10, 72, 99, 1)",

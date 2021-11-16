2cbe39bb8f1a2ad83942b86055f1b04c2bf99f86
Mariana Moreira <marianaccmoreira@gmail.com>
2021-11-16 09:57:47 +0000

    add categories services

--- src/shared/services/category.js
+++ src/shared/services/category.js
@@ -7,6 +7,27 @@ const getMany = async (params) => {
   return res ? res.data : { categories: [], totalPages: 0 };
 };
 
+const post = async (payload) => {
+  const res = await api().post("/categories", payload);
+
+  return res.data;
+};
+
+const put = async (id, payload) => {
+  const res = await api().put(`/categories/${id}`, payload);
+
+  return res.data;
+};
+
+const deleteOne = async (id) => {
+  const res = await api().delete(`/categories/${id}`);
+
+  return res.data;
+};
+
 export default {
-  getMany
+  getMany,
+  post,
+  put,
+  deleteOne
 };

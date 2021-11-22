8efa3284b381d741f59cd34ef7d06899addf0665
Mariana Moreira <marianaccmoreira@gmail.com>
2021-11-22 11:12:23 +0000

    fix login wrong pass error

--- src/shared/recoil/auth.js
+++ src/shared/recoil/auth.js
@@ -4,17 +4,15 @@ import authService from "shared/services/auth";
 // utils
 import { setJWT, clearJWT } from "shared/utils/jwt";
 
-export const signIn = async (data) => {
+export const signIn = async (payload) => {
   try {
-    const res = await authService.signIn(data);
-
-    const { token, user } = res.data;
+    const { token, user, message } = await authService.signIn(payload);
 
     if (token && user) {
       setJWT(token);
 
       return { success: true };
-    } else return res;
+    } else return message;
   } catch (error) {
     throw error;
   }
--- src/views/auth/signIn/components/form/form.js
+++ src/views/auth/signIn/components/form/form.js
@@ -1,5 +1,5 @@
 // base
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 // libraries
@@ -28,9 +28,12 @@ import { isLoggedIn } from "shared/recoil/atoms";
 const SignInForm = ({ onSubmitSuccess }) => {
   const setLoggedIn = useSetRecoilState(isLoggedIn);
 
+  const [generalError, setGeneralError] = useState("");
+
   const {
     control,
     handleSubmit,
+    watch,
     formState: { errors }
   } = useForm({
     mode: "onBlur",
@@ -40,6 +43,8 @@ const SignInForm = ({ onSubmitSuccess }) => {
     resolver: yupResolver(schemaSignIn)
   });
 
+  const password = watch("password");
+
   const onSubmit = async ({ password }) => {
     const res = await signIn({ password });
 
@@ -47,16 +52,22 @@ const SignInForm = ({ onSubmitSuccess }) => {
       setLoggedIn(true);
 
       onSubmitSuccess();
+    } else {
+      setGeneralError(res);
     }
   };
+
+  useEffect(() => {
+    if (password) setGeneralError("");
+  }, [password]);
 
   return (
     <Grid container justifyContent="center">
       <Grid item xs={12} sm={4}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <StyledInputTextField
-            error={Boolean(errors.password?.message)}
-            helperText={errors.password?.message}
+            error={Boolean(errors.password?.message) || Boolean(generalError)}
+            helperText={errors.password?.message || generalError}
             control={control}
             label="Password"
             name="password"

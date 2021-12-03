32f712d577c9fa29d248705159d5c62c8b49bca5
Mariana Moreira <marianaccmoreira@gmail.com>
2021-12-03 21:44:28 +0000

    update styles

---
+++ src/styles/global.js
@@ -0,0 +1,47 @@
+// libraries
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.colours.background};
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+    color: ${({ theme }) => theme.colours.black};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
+  }
+
+  .icon > svg {
+    width: 1.1rem;
+  }
+  
+  .MuiPaper-root {
+    border-radius: 0 !important;
+    color: ${({ theme }) => `${theme.colours.black} !important`};
+  }
+
+  .MuiFormControl-root {
+    display: block !important;
+  }
+  
+  .MuiFormHelperText-root {
+    margin-left: 10px !important;
+  }
+
+  .MuiPickersDay-root {
+    color: ${({ theme }) => `${theme.colours.black} !important`};
+  }
+
+  .MuiDivider-root {
+    border-color: ${({ theme }) => `${theme.colours.grey} !important`};
+  }
+  
+  .Mui-selected {
+    background-color: ${({ theme }) => theme.colours.primary} !important;
+    color: ${({ theme }) => `${theme.colours.white} !important`};
+  }
+  
+  .Mui-error, .error {
+    color: ${({ theme }) => theme.colours.error} !important;
+  }
+`;

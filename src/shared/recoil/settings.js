c2c2a90f3c81199f76df2d48468811a02ca3a810
Mariana Moreira <marianaccmoreira@gmail.com>
2021-12-09 21:22:39 +0000

    add recoil settings

---
+++ src/shared/recoil/settings.js
@@ -0,0 +1,21 @@
+// api
+import settingsService from "shared/services/settings";
+
+// utils
+import { setUserSettings } from "shared/utils/settings";
+
+export const saveSettings = async (user) => {
+  try {
+    const { data } = await settingsService.getOne(user);
+
+    if (data) {
+      setUserSettings(data.settings);
+    } else {
+      const settings = await settingsService.post(user);
+
+      setUserSettings(settings.data.settings);
+    }
+  } catch (error) {
+    throw error;
+  }
+};

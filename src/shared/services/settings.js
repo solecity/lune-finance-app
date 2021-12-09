f30ba1b61c1efbbdeb28d32d02abc4be479a992f
Mariana Moreira <marianaccmoreira@gmail.com>
2021-12-09 21:23:28 +0000

    add settings api call

--- src/shared/services/settings.js
+++ src/shared/services/settings.js
@@ -1,17 +1,28 @@
 // libraries
 import api from "./api";
 
-const get = async () => {
+const getDefault = async () => {
   const res = await api().get("/settings");
 
   return res.data;
 };
 
-const patch = async (payload) => {
-  console.log({ payload });
-  const res = await api().patch("/settings", payload);
+const getOne = async (id) => {
+  const res = await api().get(`/settings/${id}`);
 
   return res.data;
 };
 
-export default { get, patch };
+const post = async (id) => {
+  const res = await api().post(`/settings/${id}`);
+
+  return res.data;
+};
+
+const patch = async (id, payload) => {
+  const res = await api().patch(`/settings/${id}`, payload);
+
+  return res.data;
+};
+
+export default { getDefault, getOne, post, patch };

35f01a7a3dee7bbe279d6e5ebf5fdf31d99d3fc5
Mariana Moreira <mariana.moreira@hydradev.io>
2022-03-10 16:23:24 +0000

    update icons in navbar

--- src/config/navbar.js
+++ src/config/navbar.js
@@ -1,44 +1,47 @@
-// external components
-import { Dashboard } from "@styled-icons/boxicons-solid/Dashboard";
-import { TransferAlt } from "@styled-icons/boxicons-regular/TransferAlt";
-import { Book } from "@styled-icons/boxicons-regular/Book";
-import { Wallet } from "@styled-icons/boxicons-regular/Wallet";
-import { DollarCircle } from "@styled-icons/boxicons-regular/DollarCircle";
-import { Star } from "@styled-icons/boxicons-regular/Star";
-import { Tag } from "@styled-icons/boxicons-regular/Tag";
-import { UserCircle } from "@styled-icons/boxicons-regular/UserCircle";
-import { ShoppingBag } from "@styled-icons/boxicons-regular/ShoppingBag";
-import { BarChart } from "@styled-icons/boxicons-regular/BarChart";
-import { Calculator } from "@styled-icons/boxicons-regular/Calculator";
-import { Cog } from "@styled-icons/boxicons-regular/Cog";
-import { LogOutCircle } from "@styled-icons/boxicons-regular/LogOutCircle";
+// icons
+import {
+  Grid,
+  DoubleArrows,
+  Book,
+  Piggy,
+  DollarCircle,
+  Star,
+  Tag,
+  User,
+  Bag,
+  Chart,
+  Calculator,
+  Message,
+  Settings,
+  SignOut
+} from "shared/icons";
 
 const menuItems = [
-  { name: "overview", label: "Overview", icon: Dashboard, href: "/overview" },
+  { name: "overview", label: "Overview", icon: Grid, href: "/overview" },
   {
     name: "transactions",
     label: "Transactions",
-    icon: TransferAlt,
+    icon: DoubleArrows,
     href: "/transactions"
   },
   { name: "accounts", label: "Accounts", icon: Book, href: "/accounts" },
-  { name: "budgets", label: "Budgets", icon: Wallet, href: "/budgets" },
+  { name: "budgets", label: "Budgets", icon: Piggy, href: "/budgets" },
   { name: "debts", label: "Debts", icon: DollarCircle, href: "/debts" },
   { name: "goals", label: "Goals", icon: Star, href: "/goals" },
   { name: "categories", label: "Categories", icon: Tag, href: "/categories" },
   {
     name: "recipients",
     label: "Recipients",
-    icon: UserCircle,
+    icon: User,
     href: "/recipients"
   },
   {
     name: "shops",
     label: "Shops",
-    icon: ShoppingBag,
+    icon: Bag,
     href: "/shops"
   },
-  { name: "analytics", label: "Analytics", icon: BarChart, href: "/analytics" },
+  { name: "analytics", label: "Analytics", icon: Chart, href: "/analytics" },
   {
     name: "calculators",
     label: "Calculators",
@@ -48,8 +51,9 @@ const menuItems = [
 ];
 
 const footerItems = [
-  { name: "settings", label: "Settings", icon: Cog, href: "/settings" },
-  { name: "logout", label: "Logout", icon: LogOutCircle, href: "/signout" }
+  { name: "support", label: "Support", icon: Message, href: "/support" },
+  { name: "settings", label: "Settings", icon: Settings, href: "/settings" },
+  { name: "sign-out", label: "Sign out", icon: SignOut, href: "/sign-out" }
 ];
 
 export { menuItems, footerItems };

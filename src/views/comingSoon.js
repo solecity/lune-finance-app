2b817263f349b7bc328366bdc924ed31c39a7449
Mariana Moreira <mariana.moreira@hydradev.io>
2022-03-19 11:24:51 +0000

    coming soon page

---
+++ src/views/comingSoon.js
@@ -0,0 +1,21 @@
+// base
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <h1
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      Coming soon
+    </h1>
+  );
+};
+
+ComingSoon.propTypes = {};
+
+export default ComingSoon;

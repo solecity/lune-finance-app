ed3c7f24bad8143cfd90f3e11cd06c4de0e41f80
Mariana Moreira <mariana.moreira@hydradev.io>
2022-05-07 22:04:03 +0100

    update container styling

--- src/shared/components/container/styles.js
+++ src/shared/components/container/styles.js
@@ -2,8 +2,7 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-  margin: ${({ theme }) => theme.spacing.s10}
-    ${({ theme }) => theme.spacing.s12};
+  margin: ${({ theme }) => theme.spacing.s17};
 `;
 
 export { StyledContainer };
